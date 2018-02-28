0036da1ba5ac768abb10cb89a98efa78d2cf2f78
ymademikhanov <mademihanov.erzhan@gmail.com>
2018-02-28 15:05:51 +0600

    Added Google Maps on front with geolocation. Tested Google Directions API and some custom Google Places API wrapper.

--- src/main/MHGoogleAPITest.java
+++ src/main/MHGoogleAPITest.java
@@ -5,48 +5,30 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.List;
 
-import se.walkercrou.places.GooglePlaces;
-import se.walkercrou.places.Place;
-
-public class MHGoogleAPITest {	
+public class MHGoogleAPITest {
+	private static String googleApiRequestUrl = "https://maps.googleapis.com/maps/api/directions/json?";
+	private static String googleApiKey = "AIzaSyDdRpr14HhdbyWQkq5p2Pe0tMv2si_g4sY";
+	
 	public static void main(String[] args) throws IOException {
-		//AIzaSyDdRpr14HhdbyWQkq5p2Pe0tMv2si_g4sY
-		GooglePlaces client = new GooglePlaces("AIzaSyDdRpr14HhdbyWQkq5p2Pe0tMv2si_g4sY");
-		
-		double lat = 51.5332643;
-		double lng = -0.1281917;
-		double radius = 5000;
-		List<Place> places = client.getNearbyPlaces(lat, lng, radius, GooglePlaces.MAXIMUM_RESULTS);
-		
-		for (Place place : places) {
-			System.out.println(place.getName());
-		}
-		
-		System.out.println("Finished");
+		testing1();
 	}
 	
 	public static void testing1() throws IOException {
-		String key="AIzaSyDdRpr14HhdbyWQkq5p2Pe0tMv2si_g4sY";
-		String urlka = "https://maps.googleapis.com/maps/api/directions/json?origin=Toronto&destination=Montreal&key=" + key;
+		String urlka = googleApiRequestUrl + "origin=Farringdon&destination=WhiteChapel&mode=train&key=" + googleApiKey;
 		URL url = new URL(urlka);
 		HttpURLConnection con = (HttpURLConnection) url.openConnection();
 		con.setRequestMethod("GET");
-//		con.setRequestProperty("User-Agent", USER_AGENT);
 		
 		int responseCode = con.getResponseCode();
-		
 		if (responseCode == HttpURLConnection.HTTP_OK) {
 			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
 			String inputLine;
 			StringBuffer response = new StringBuffer();
-
 			while ((inputLine = in.readLine()) != null) {
 				response.append(inputLine);
 			}
 			in.close();
-			
 			System.out.println(response);
 		} else {
 			System.out.println("fail");
