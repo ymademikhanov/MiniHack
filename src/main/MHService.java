39fa83e1da191ae778e39377c46520a1e4a12f66
ymademikhanov <mademihanov.erzhan@gmail.com>
2018-03-01 18:00:59 +0600

    Added Google Places suggestions search. Not fully working.

--- src/main/MHService.java
+++ src/main/MHService.java
@@ -1,5 +1,14 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -10,18 +19,55 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 import javax.ws.rs.core.Response.Status;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-@Path("/todo")
+import com.google.gson.Gson;
+
+
+@Path("/findPlace")
 public class MHService {
 
+	private static String googlePlacesUrl = "https://maps.googleapis.com/maps/api/place/autocomplete/json?key=AIzaSyC_TEVCna6Qfoe54Tv7qELwCjZ70GpYHfU";
+	
 	public MHService() {
 	}
 	
 	@GET
-	public Response getList(@QueryParam("id") String id) {
+	public Response getList(@QueryParam("input") String input) throws IOException {
 		String result;
+		String request = googlePlacesUrl + "&input=" + input;
+		URL requestUrl = new URL(request);
+		HttpURLConnection con = (HttpURLConnection) requestUrl.openConnection();
+		con.setRequestMethod("GET");
 		
-		return null;
+		int responseCode = con.getResponseCode();
+		if (responseCode == HttpURLConnection.HTTP_OK) {
+			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			String inputLine;
+			StringBuffer response = new StringBuffer();
+			while ((inputLine = in.readLine()) != null) {
+				response.append(inputLine);
+			}
+			in.close();
+//			System.out.println(response);
+			result = response.toString();
+			
+			JSONObject obj = new JSONObject(result);
+			JSONArray obj2 = obj.getJSONArray("predictions");
+			
+			List<String> list = new ArrayList<String>();
+			
+			for (int i=0; i< obj2.length(); i++) {
+			    list.add(obj2.getJSONObject(i).getString("description"));
+			}
+			
+			System.out.println(list);
+			result = new Gson().toJson(list);
+		} else {
+			result = "fail";
+		}
+		return Response.ok(result).build();
 	}
 	
 	@POST
