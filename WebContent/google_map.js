a636d5fbf87b837d2315e3ff0f0a5390eab0dbfb
ymademikhanov <mademihanov.erzhan@gmail.com>
2018-03-11 23:52:56 +0600

    Fixed GoogleMap JS class from exposing its fields.

--- WebContent/google_map.js
+++ WebContent/google_map.js
@@ -1,26 +1,26 @@
 function GoogleMap(elementId) {
 	// A helper converting function.
-	this.convert = (lat, lng) => new google.maps.LatLng(lat, lng);
+	var convert = (lat, lng) => new google.maps.LatLng(lat, lng);
 	
 	// Properties.
-	this.center = this.convert(51.508742, -0.120850);
-	this.map = new google.maps.Map(document.getElementById(elementId), { center : this.center, zoom : 10 } );
-	this.marker = new google.maps.Marker({
-        position: this.center,
-        map: this.map
+	var center = convert(51.508742, -0.120850);
+	var map = new google.maps.Map(document.getElementById(elementId), { center : center, zoom : 10 } );
+	var marker = new google.maps.Marker({
+        position: center,
+        map: map
     });
 		
 	// Methods.
 	this.changeCenter = function(lat, lng) {
-		this.center = this.convert(lat, lng);
-		this.map.setCenter(this.center);
-		this.marker.setPosition(this.center);
+		center = convert(lat, lng);
+		map.setCenter(center);
+		marker.setPosition(center);
 	}
 	
 //	Helper function for debugging.
 	this.print = function() {
-		console.log(this.map);
-		console.log(this.properties);
-		console.log(this.marker);
+		console.log(properties);
+		console.log(marker);
+		console.log(map);
 	}
 }
