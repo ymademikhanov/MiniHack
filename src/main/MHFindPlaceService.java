0dcff23a83a5d91f6ee5131c08f6379d0e55652d
ymademikhanov <mademihanov.erzhan@gmail.com>
2018-05-03 03:41:06 +0600

    Added a little logging.

--- src/main/MHFindPlaceService.java
+++ src/main/MHFindPlaceService.java
@@ -5,19 +5,23 @@ import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
+import org.apache.log4j.Logger;
 import com.google.gson.Gson;
 
 @Path("/findPlace")
 public class MHFindPlaceService {
 	private Autocomplete autocompleter;
+	static Logger log = Logger.getLogger(MHFindPlaceService.class.getName());
 	
 	public MHFindPlaceService(Autocomplete autocompleter) {
+		log.info("initialised.");
 		this.autocompleter = autocompleter;
 	}
 	
 	@GET
 	public Response getList(@QueryParam("input") String input) {
-		System.out.println("got query");
+		
+		log.info("got query.");
 		String result = new Gson().toJson(this.autocompleter.autocomplete(input));
 		
 		
